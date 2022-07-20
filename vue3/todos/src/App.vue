db3ef86752fd809880b5a4fafcb477253432c3c0
light-filled <102566048+light-filled@users.noreply.github.com>
2022-07-20 23:04:34 +0800

    Add files via upload

---
+++ vue3/todos/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>App根组件----{{number}}</h1>
+  <button @click="number += 1">+1</button>
+  <left v-model:count="number"></left>
+</template>
+
+<script>
+import Left from './components/Left.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      number: 0
+    }
+  },
+  components: {
+    Left
+  }
+}
+</script>
---
+++ vue3/todos/src/components/Left.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>Left组件 ----{{count}}</h3>
+  <button @click="add">+1</button>
+
+</template>
+
+<script>
+export default {
+  data() {},
+  props: ['count'],
+  emits: ['update:count'],
+  methods: {
+    add() {
+      this.$emit('update:count', this.count + 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
